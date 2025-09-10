b3149d86c67a3e885822626cc64fcab588a0fc84
Trang Vu <vulinhtrang2305@gmail.com>
2025-09-10 23:27:35 +0700

    feat: user feedback in landing page

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BannerLayout from '~/layouts/BannerLayout';
 import CategoryBannerLayout from '~/layouts/CategoryBannerLayout';
+import UserFeedbackLayout from '~/layouts/UserFeedbackLayout';
 
 const Page = () => {
   return (
    <>
     <BannerLayout/>
     <CategoryBannerLayout/>
+    <UserFeedbackLayout/>
    </>
   );
 };
