6657d57c221e934c31b7c639618afdfa5e7b6c66
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-09-25 16:31:16 +0700

    feat: responsive footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,9 +3,9 @@ import { FaFacebookF, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer className='bg-blue-900 text-white px-10 py-12 rounded-xl'>
+    <footer className='bg-blue-900 text-white px-6 sm:px-10 py-12 rounded-xl'>
       <div className='max-w-7xl mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-8 mb-10'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-10'>
           <div>
             <div className='flex items-center space-x-2 mb-4'>
               <span className='font-bold text-4xl'>F-FITNESS</span>
@@ -81,15 +81,15 @@ function Footer() {
           </div>
         </div>
 
-        <div className='border-t border-blue-800 pt-6 flex flex-col md:flex-row md:items-center md:justify-between text-sm'>
-          <div className='flex space-x-5 mb-4 md:mb-0'>
+        <div className='border-t border-blue-800 pt-6 flex flex-col sm:flex-row sm:items-center sm:justify-between text-sm'>
+          <div className='flex space-x-5 mb-4 sm:mb-0'>
             <FaTwitter className='hover:text-gray-300 cursor-pointer' />
             <FaFacebookF className='hover:text-gray-300 cursor-pointer' />
             <FaInstagram className='hover:text-gray-300 cursor-pointer' />
             <FaGithub className='hover:text-gray-300 cursor-pointer' />
           </div>
 
-          <div className='flex space-x-6 mb-4 md:mb-0'>
+          <div className='flex space-x-6 mb-4 sm:mb-0'>
             <a href='#'>Privacy Policy</a>
             <a href='#'>Terms & Conditions</a>
             <a href='#'>Support</a>
