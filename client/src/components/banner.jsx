fd0b4b5792e930be1fd6c5b1f19cdb90e3d0f2eb
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-09-25 16:20:02 +0700

    fix: banner component and responsive

--- client/src/components/banner.jsx
+++ client/src/components/banner.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 import { Button } from '~/components/ui/button';
@@ -6,11 +7,23 @@ import banner from '../assets/banner-1.png';
 
 function Banner() {
   return (
-    <div className='relative text-center p-19'>
-      <img src={banner} alt='Healthy Lifestyle' className='w-full h-auto' />
-      <Button className='absolute top-7/9 left-7/9 transform -translate-x-10/50 -translate-y-/50 py-8 px-8 bg-[#F5F2EC] text-[#3067B6] rounded-2xl cursor-pointer text-xl font-bold'>
-        Join Now
-      </Button>
+    <div className='relative text-center p-8 lg:p-16'>
+      <motion.img
+        src={banner}
+        alt='Healthy Lifestyle'
+        className='w-full h-auto'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      />
+      <motion.div
+        className='absolute bottom-30 right-50 py-4 px-6 text-[#3067B6] rounded-2xl cursor-pointer text-xl font-bold'
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8 }}
+      >
+        <Button className='w-full lg:w-auto'>Join Now</Button>
+      </motion.div>
     </div>
   );
 }
