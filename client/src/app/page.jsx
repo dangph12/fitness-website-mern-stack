4c55eb8c7d6fb8fec13ccdcc5540aee245c6890a
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-09-25 15:58:57 +0700

    feat: add intro section intro icon in page

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -4,6 +4,8 @@ import Banner from '~/components/banner';
 import CategoryBanner from '~/components/category-banner';
 import Features from '~/components/features';
 import Feedback from '~/components/feedback';
+import IntroIcon from '~/components/intro-icon';
+import IntroSection from '~/components/intro-section';
 import PlanCourses from '~/components/plan-courses';
 
 function Page() {
@@ -11,6 +13,8 @@ function Page() {
     <div>
       <Banner />
       <Features />
+      <IntroSection />
+      <IntroIcon />
       <CategoryBanner />
       <Feedback />
       <PlanCourses />
