1158b61e20c06d61bebe590467d154fe40c59873
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-09-29 10:46:46 +0700

    chore: add fields for food

--- server/src/modules/foods/food-type.ts
+++ server/src/modules/foods/food-type.ts
@@ -1,8 +1,10 @@
 export interface IFood {
   title: string;
   image: string;
+  unit: number;
   calories: number;
   proteins: number;
   carbs: number;
   fats: number;
+  category: 'Meat' | 'Egg' | 'Fruits & Vegetables';
 }
