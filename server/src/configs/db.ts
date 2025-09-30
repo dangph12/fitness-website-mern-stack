ecf92e5c82b9e1c742e5a9dc123b2d1835aa5f4a
namdng09 <namduong090104@gmail.com>
2025-09-30 11:28:21 +0700

    refactor: add new infor for the connection database

--- server/src/configs/db.ts
+++ server/src/configs/db.ts
@@ -6,8 +6,10 @@ const db = process.env.MONGO_URI as string;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
-    console.log('MongoDB is Connected...');
+    const conn = await mongoose.connect(db);
+    const hostPort = `${conn.connection.host}:${conn.connection.port}`;
+    const dbName = conn.connection.name;
+    console.log(`MongoDB is Connected: ${hostPort}/${dbName}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
