5e8fe966c13d06f8afdb82c6546cbf0191c67c93
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-01 20:32:35 +0700

    feat: create page for nutrition page

---
+++ client/src/app/nutrition/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NutriBanner from '~/components/nutri-banner';
+import NutriBanner2 from '~/components/nutri-banner2';
+
+function page() {
+  return (
+    <div>
+      <NutriBanner />
+      <NutriBanner2 />
+    </div>
+  );
+}
+
+export default page;
