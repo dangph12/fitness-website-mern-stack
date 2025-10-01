c41a141e0932ec55723ee7020fb18e6a1ce8ab18
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-02 01:12:06 +0700

    chore: button edit and delete

--- client/src/components/meal-list.jsx
+++ client/src/components/meal-list.jsx
@@ -74,15 +74,16 @@ const MealsList = () => {
               <h2 className='text-xl font-semibold'>{meal.title}</h2>
               <p className='text-sm text-gray-500 mt-2'>{meal.mealType}</p>
 
-              <div className='mt-4 flex justify-between space-x-2'>
+              <div className='mt-4 flex justify-between space-x-4'>
                 <button
-                  className='w-full bg-yellow-400 text-white px-3 py-1 rounded hover:bg-yellow-500'
+                  className='w-full bg-gradient-to-r from-yellow-400 to-yellow-500 text-white px-6 py-2 rounded-lg shadow-md hover:shadow-lg transform transition-all duration-200 hover:scale-105'
                   onClick={() => handleSelectMeal(meal)}
                 >
                   Edit
                 </button>
+
                 <button
-                  className='w-full bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600'
+                  className='w-full bg-gradient-to-r from-red-500 to-red-600 text-white px-6 py-2 rounded-lg shadow-md hover:shadow-lg transform transition-all duration-200 hover:scale-105'
                   onClick={() => handleDeleteMeal(meal._id)}
                 >
                   Delete
