1c3f038adff949a8a19fff4b6bb34a031c0af349
namdng09 <namduong090104@gmail.com>
2025-10-01 20:51:21 +0700

    feat: update field in meal type

--- server/src/modules/meals/meal-model.ts
+++ server/src/modules/meals/meal-model.ts
@@ -6,7 +6,11 @@ const MealSchema = new Schema<IMeal>(
   {
     title: { type: String, required: true },
     image: { type: String, default: '' },
-    mealType: { type: String, required: true },
+    mealType: {
+      type: String,
+      enum: ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Brunch', 'Dessert'],
+      required: true
+    },
     quantity: { type: Number, required: true },
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     foods: [{ type: Schema.Types.ObjectId, ref: 'Food' }]
--- server/src/modules/meals/meal-type.ts
+++ server/src/modules/meals/meal-type.ts
@@ -3,7 +3,7 @@ import { Types } from 'mongoose';
 export interface IMeal {
   title: string;
   image: string;
-  mealType: string;
+  mealType: 'Breakfast' | 'Lunch' | 'Dinner' | 'Snack' | 'Brunch' | 'Dessert';
   quantity: number;
   userId: Types.ObjectId;
   foods: [Types.ObjectId];
