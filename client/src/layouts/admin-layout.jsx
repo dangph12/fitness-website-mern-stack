caf7dfeb519adb5e1d75500b2379ba3ff35e9d47
lnvh <lenguyenviethung1509@gmail.com>
2025-10-02 19:42:25 +0700

    feat: create admin layout

--- client/src/layouts/admin-layout.jsx
+++ client/src/layouts/admin-layout.jsx
@@ -1,16 +1,57 @@
-import { useTheme } from 'next-themes';
 import React from 'react';
-import { Outlet } from 'react-router';
 
-import { Toaster } from '~/components/ui/sonner';
+import { AdminSidebar } from '../components/admin-sidebar.jsx';
+import { ProfileDropdown } from '../components/profile-dropdown';
+import { SearchInput } from '../components/search';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from '../components/ui/breadcrumb';
+import { ModeToggle } from '../components/ui/darkmode';
+import { Separator } from '../components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger
+} from '../components/ui/sidebar';
 
-const AdminLayout = () => {
-  const { theme } = useTheme();
+const AdminLayout = ({ children }) => {
   return (
-    <div>
-      <Outlet />
-      <Toaster position='top-right' theme={theme} />
-    </div>
+    <SidebarProvider>
+      <AdminSidebar />
+      <SidebarInset>
+        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b'>
+          <div className='flex items-center gap-2 px-4'>
+            <SidebarTrigger className='-ml-1' />
+            <Separator orientation='vertical' className='mr-2 h-4' />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className='hidden md:block'>
+                  <BreadcrumbLink href='/admin'>Admin Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className='hidden md:block' />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Fitness Management</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+
+          <div className='flex items-center gap-4 ml-auto px-4'>
+            <div className='hidden md:block'>
+              <SearchInput />
+            </div>
+            <ModeToggle />
+            <ProfileDropdown />
+          </div>
+        </header>
+        <div className='flex flex-1 flex-col p-6'>{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
