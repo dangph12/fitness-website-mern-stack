78a408f56cd46cae880288697acb9138651b7d31
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-02 19:42:54 +0700

    feat: add equipment in excersice list

--- client/src/app/exercises/page.jsx
+++ client/src/app/exercises/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import EquipmentList from '~/components/equipment-list';
 import MuscleList from '~/components/muscle-list';
 
 function page() {
   return (
     <div>
       <MuscleList />
+      <EquipmentList />
     </div>
   );
 }
