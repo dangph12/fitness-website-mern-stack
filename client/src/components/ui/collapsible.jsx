f83524e79293af3e9ff434229b0f180383d20ab9
lnvh <lenguyenviethung1509@gmail.com>
2025-10-02 19:35:27 +0700

    feat: create collapsible ui

---
+++ client/src/components/ui/collapsible.jsx
@@ -0,0 +1,10 @@
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
+import * as React from 'react';
+
+const Collapsible = CollapsiblePrimitive.Root;
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+export { Collapsible, CollapsibleContent, CollapsibleTrigger };
