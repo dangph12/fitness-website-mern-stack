8e1f485be898db1b3eca7849f1a545e24e9013cf
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-02 22:09:31 +0700

    fix: eqipment list

--- client/src/components/equipment-list.jsx
+++ client/src/components/equipment-list.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchEquipments } from '~/store/features/equipment-slice';
 
 function EquipmentList() {
   const { equipments, loading, error } = useSelector(state => state.equipments);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEquipments());
+  }, [dispatch]);
+
+  if (loading) return <div className='text-center'>Loading...</div>;
+  if (error) return <div className='text-center text-red-500'>{error}</div>;
+
   return (
     <div className='container mx-auto p-6'>
-      {loading && <div className='text-center'>Loading...</div>}
-      {error && <div className='text-center text-red-500'>{error}</div>}
-
       <div className='mb-8'>
         <h2 className='text-6xl font-bold text-black'>Equipment Database</h2>
         <p className='text-lg text-black-400 mt-2'>
@@ -25,14 +33,13 @@ function EquipmentList() {
         {equipments.map(equipment => (
           <div
             key={equipment._id}
-            className='bg-white p-4 rounded-full text-center hover:scale-105 transform transition duration-300 ease-in-out'
+            className='bg-white p-4 rounded-lg text-center hover:scale-105 transform transition duration-300 ease-in-out'
           >
             <img
               src={equipment.image}
               alt={equipment.title}
               className='w-24 h-24 object-cover rounded-full mb-2 transition-transform duration-300 ease-in-out mx-auto'
             />
-
             <h3 className='text-lg font-medium text-blue-600'>
               {equipment.title}
             </h3>
