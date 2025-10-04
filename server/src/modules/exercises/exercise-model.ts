12dab8566463b3879a66198d8065f8fbc95ece4a
namdng09 <namduong090104@gmail.com>
2025-10-04 16:35:22 +0700

    feat: update model with new enum

--- server/src/modules/exercises/exercise-model.ts
+++ server/src/modules/exercises/exercise-model.ts
@@ -5,11 +5,41 @@ import { IExercise } from './exercise-type';
 const ExerciseSchema = new Schema<IExercise>(
   {
     title: { type: String, required: true },
-    difficulty: { type: String, required: true },
+    difficulty: {
+      type: String,
+      enum: ['Beginner', 'Intermediate', 'Advanced'],
+      required: true
+    },
+    exerciseType: {
+      type: String,
+      enum: [
+        'Strength',
+        'Stretching',
+        'Power',
+        'Olympic',
+        'Explosive',
+        'Mobility',
+        'Dynamic',
+        'Yoga'
+      ],
+      required: true
+    },
     tutorialVideo: { type: String, default: '' },
     instructions: { type: String, required: true },
-    muscles: [{ type: Schema.Types.ObjectId, ref: 'Muscle' }],
-    equipment: [{ type: Schema.Types.ObjectId, ref: 'Equipment' }]
+    muscles: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Muscle',
+        required: true
+      }
+    ],
+    equipments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Equipment',
+        required: true
+      }
+    ]
   },
   { timestamps: true }
 );
--- server/src/modules/exercises/exercise-type.ts
+++ server/src/modules/exercises/exercise-type.ts
@@ -3,8 +3,9 @@ import { Types } from 'mongoose';
 export interface IExercise {
   title: string;
   difficulty: string;
+  exerciseType: string;
   tutorialVideo: string;
   instructions: string;
   muscles: [Types.ObjectId];
-  equipment: [Types.ObjectId];
+  equipments: [Types.ObjectId];
 }
