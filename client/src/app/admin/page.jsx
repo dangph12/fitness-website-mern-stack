7d36b6f136a0dc75682a5ec54caecc12a76cf040
lnvh <lenguyenviethung1509@gmail.com>
2025-10-06 10:17:08 +0700

    feat: create admin page

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import AdminLayout from '~/layouts/admin-layout';
 
 import Dashboard from './dashboard/page';
+import Users from './manage-users/page';
 function Page() {
   return (
     <div>
       <AdminLayout>
         <Dashboard />
+        {/* <Users /> */}
       </AdminLayout>
     </div>
   );
