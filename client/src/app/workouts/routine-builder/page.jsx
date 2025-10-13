027bb2a122fb20417423bf0848c1c61aada81819
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-14 02:16:28 +0700

    fix: page in workout rountine

--- client/src/app/workouts/routine-builder/page.jsx
+++ client/src/app/workouts/routine-builder/page.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CreateWorkout from '~/components/create-workout';
-import ExerciseLibrary from '~/components/exercise-library';
 
 function Page() {
-  return (
-    <div className='flex space-x-8 p-6'>
-      <div className='w-5/6'>
-        <CreateWorkout />
-      </div>
+  const [selectedExercises, setSelectedExercises] = useState([]);
 
-      <div className='w-1/2'>
-        <ExerciseLibrary />
-      </div>
+  return (
+    <div>
+      <CreateWorkout
+        selectedExercises={selectedExercises}
+        setSelectedExercises={setSelectedExercises}
+      />
     </div>
   );
 }
