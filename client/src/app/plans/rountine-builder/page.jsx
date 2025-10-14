28bc8af5ba7d48643d9f9dd5cb05b97c90333d1f
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-14 23:17:06 +0700

    feat: plan page

---
+++ client/src/app/plans/rountine-builder/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CreatePlan from '~/components/create-plan';
+
+function page() {
+  return (
+    <div>
+      <CreatePlan />
+    </div>
+  );
+}
+
+export default page;
