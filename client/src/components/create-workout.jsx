bf809e554f9580f54d5fd3dec858c77a54722dc1
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-14 20:15:56 +0700

    fix: create workout

--- client/src/components/create-workout.jsx
+++ client/src/components/create-workout.jsx
@@ -7,73 +7,63 @@ import { createWorkout } from '~/store/features/workout-slice';
 import ExerciseLibrary from './exercise-library';
 
 const CreateWorkout = () => {
-  const [days, setDays] = useState([{ dayName: 'Day 1', exercises: [] }]);
+  const [exercises, setExercises] = useState([]);
   const userId = useSelector(state => state.auth.user.id);
-  const [selectedDay, setSelectedDay] = useState(0);
 
   const dispatch = useDispatch();
   const { loading, error } = useSelector(state => state.workouts);
 
-  const addDay = () => {
-    const newDay = { dayName: `Day ${days.length + 1}`, exercises: [] };
-    setDays([...days, newDay]);
-  };
-
   const handleAddExercise = exercise => {
-    const updatedDays = [...days];
-
-    const exerciseExists = updatedDays[selectedDay].exercises.some(
-      e => e.exercise._id === exercise._id
-    );
+    const exerciseExists = exercises.some(e => e.exercise._id === exercise._id);
 
     if (!exerciseExists) {
-      updatedDays[selectedDay].exercises.push({
-        exercise: { _id: exercise._id, title: exercise.title },
-        sets: [1]
-      });
-
-      setDays(updatedDays);
+      setExercises([
+        ...exercises,
+        {
+          exercise: { _id: exercise._id, title: exercise.title },
+          sets: [1]
+        }
+      ]);
     }
   };
 
   const handleRemoveExercise = exerciseIndex => {
-    const updatedDays = [...days];
-    updatedDays[selectedDay].exercises.splice(exerciseIndex, 1);
-    setDays(updatedDays);
+    const updatedExercises = [...exercises];
+    updatedExercises.splice(exerciseIndex, 1);
+    setExercises(updatedExercises);
   };
 
   const handleInputChange = (exerciseIndex, setIndex, value) => {
-    const updatedDays = [...days];
-    updatedDays[selectedDay].exercises[exerciseIndex].sets[setIndex] = value;
-    setDays(updatedDays);
+    const updatedExercises = [...exercises];
+    updatedExercises[exerciseIndex].sets[setIndex] = value;
+    setExercises(updatedExercises);
   };
 
   const handleAddSet = exerciseIndex => {
-    const updatedDays = [...days];
-    updatedDays[selectedDay].exercises[exerciseIndex].sets.push(1);
-    setDays(updatedDays);
+    const updatedExercises = [...exercises];
+    updatedExercises[exerciseIndex].sets.push(1);
+    setExercises(updatedExercises);
   };
 
   const handleRemoveSet = (exerciseIndex, setIndex) => {
-    const updatedDays = [...days];
-    updatedDays[selectedDay].exercises[exerciseIndex].sets.splice(setIndex, 1);
-    setDays(updatedDays);
+    const updatedExercises = [...exercises];
+    updatedExercises[exerciseIndex].sets.splice(setIndex, 1);
+    setExercises(updatedExercises);
   };
 
   const handleSubmitWorkout = () => {
     const workoutData = {
-      title: `Workout Day ${selectedDay + 1}`,
+      title: 'My Workout',
       image: '',
       isPublic: true,
       user: userId,
-      exercises: days[selectedDay].exercises.map(exercise => ({
+      exercises: exercises.map(exercise => ({
         exercise: exercise.exercise._id,
         sets: exercise.sets
       }))
     };
 
     console.log('Workout data:', workoutData);
-
     dispatch(createWorkout(workoutData));
   };
 
@@ -84,13 +74,6 @@ const CreateWorkout = () => {
 
         <div className='flex justify-between mb-4'>
           <button
-            onClick={addDay}
-            className='bg-blue-600 text-white px-4 py-2 rounded-md'
-          >
-            + Add Day
-          </button>
-
-          <button
             onClick={handleSubmitWorkout}
             className='bg-blue-600 text-white px-4 py-2 rounded-md flex items-center'
             disabled={loading}
@@ -100,90 +83,58 @@ const CreateWorkout = () => {
           </button>
         </div>
 
-        {days.map((day, dayIndex) => (
-          <div key={dayIndex} className='mt-4 border rounded-md p-4 mb-4'>
-            <div className='flex justify-between items-center mb-4'>
-              <div className='flex items-center'>
-                <span
-                  onClick={() => setSelectedDay(dayIndex)}
-                  className={`text-lg font-semibold cursor-pointer ${
-                    selectedDay === dayIndex ? 'text-blue-600' : 'text-gray-600'
-                  }`}
-                >
-                  {day.dayName}
-                </span>
+        {exercises.length > 0 ? (
+          exercises.map((exercise, exerciseIndex) => (
+            <div
+              key={exerciseIndex}
+              className='p-4 bg-gray-100 rounded-md mb-4'
+            >
+              <div className='flex items-center justify-between mb-3'>
+                <span>{exercise.exercise.title}</span>
                 <button
-                  onClick={() => handleRemoveExercise(dayIndex)}
-                  className='ml-4 text-red-500'
+                  onClick={() => handleRemoveExercise(exerciseIndex)}
+                  className='text-red-500'
                 >
                   <FaTrash />
                 </button>
               </div>
-              <span className='text-sm text-gray-500'>
-                Est. 0 min | {day.exercises.length} exercises
-              </span>
-            </div>
 
-            {day.exercises.length === 0 ? (
-              <p>This day is empty. Add exercises from the library.</p>
-            ) : (
-              <div className='space-y-4'>
-                {day.exercises.map((exercise, exerciseIndex) => (
-                  <div
-                    key={exerciseIndex}
-                    className='p-4 bg-gray-100 rounded-md'
-                  >
-                    <div className='flex items-center justify-between mb-3'>
-                      <span>{exercise.exercise.title}</span>
-                      <button
-                        onClick={() => handleRemoveExercise(exerciseIndex)}
-                        className='text-red-500'
-                      >
-                        <FaTrash />
-                      </button>
-                    </div>
-                    <div className='grid grid-cols-4 gap-4'>
-                      <span className='font-semibold'>Set</span>
-                      <span className='font-semibold'>Reps</span>
-                    </div>
-                    {exercise.sets.map((set, setIndex) => (
-                      <div key={setIndex} className='grid grid-cols-4 gap-4'>
-                        <span>{setIndex + 1}</span>
-                        <input
-                          type='number'
-                          value={set}
-                          onChange={e =>
-                            handleInputChange(
-                              exerciseIndex,
-                              setIndex,
-                              e.target.value
-                            )
-                          }
-                          className='p-2 border rounded-md'
-                          placeholder='Reps'
-                        />
-                        <button
-                          onClick={() =>
-                            handleRemoveSet(exerciseIndex, setIndex)
-                          }
-                          className='text-red-500'
-                        >
-                          <FaTrash />
-                        </button>
-                      </div>
-                    ))}
-                    <button
-                      onClick={() => handleAddSet(exerciseIndex)}
-                      className='mt-2 text-blue-600'
-                    >
-                      + Add Set
-                    </button>
-                  </div>
-                ))}
+              <div className='grid grid-cols-4 gap-4'>
+                <span className='font-semibold'>Set</span>
+                <span className='font-semibold'>Reps</span>
               </div>
-            )}
-          </div>
-        ))}
+
+              {exercise.sets.map((set, setIndex) => (
+                <div key={setIndex} className='grid grid-cols-4 gap-4'>
+                  <span>{setIndex + 1}</span>
+                  <input
+                    type='number'
+                    value={set}
+                    onChange={e =>
+                      handleInputChange(exerciseIndex, setIndex, e.target.value)
+                    }
+                    className='p-2 border rounded-md'
+                    placeholder='Reps'
+                  />
+                  <button
+                    onClick={() => handleRemoveSet(exerciseIndex, setIndex)}
+                    className='text-red-500'
+                  >
+                    <FaTrash />
+                  </button>
+                </div>
+              ))}
+              <button
+                onClick={() => handleAddSet(exerciseIndex)}
+                className='mt-2 text-blue-600'
+              >
+                + Add Set
+              </button>
+            </div>
+          ))
+        ) : (
+          <p>Please select exercises from the library.</p>
+        )}
       </div>
 
       <div className='w-1/3 border-l p-4 rounded-lg shadow-lg'>
