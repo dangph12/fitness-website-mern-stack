d956bad6eeee21cc9c81cb0a1a856a0c43604ebf
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-16 06:34:55 +0700

    feat: workout list pagination

--- client/src/components/workouts-list.jsx
+++ client/src/components/workouts-list.jsx
@@ -1,33 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaEdit, FaTrash } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router';
 import { toast } from 'sonner';
 
-import { fetchEquipments } from '~/store/features/equipment-slice';
-import { fetchMuscles } from '~/store/features/muscles-slice';
-import { deleteWorkout, fetchWorkouts } from '~/store/features/workout-slice';
+import { fetchExerciseById } from '~/store/features/exercise-slice';
+import { fetchWorkouts } from '~/store/features/workout-slice';
+
+import logo from '../assets/logo.png';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from './ui/pagination';
 
 const WorkoutList = () => {
   const dispatch = useDispatch();
-  const { workouts, loading, error } = useSelector(state => state.workouts);
-  const { muscles } = useSelector(state => state.muscles);
-  const { equipments } = useSelector(state => state.equipments);
+  const {
+    workouts,
+    loading,
+    error,
+    totalPages = 1
+  } = useSelector(state => state.workouts);
+  const { exercises, currentExercise } = useSelector(state => state.exercises);
   const navigate = useNavigate();
 
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
-    dispatch(fetchWorkouts());
-    dispatch(fetchMuscles());
-    dispatch(fetchEquipments());
-  }, [dispatch]);
+    dispatch(fetchWorkouts({ page: currentPage }));
+  }, [dispatch, currentPage]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    workouts.forEach(workout => {
+      workout.exercises.forEach(exercise => {
+        if (!exercises.some(ex => ex._id === exercise.exercise)) {
+          dispatch(fetchExerciseById(exercise.exercise));
+        }
+      });
+    });
+  }, [dispatch, workouts, exercises]);
 
-  if (error) {
-    return <div className='text-red-500'>{error}</div>;
-  }
+  const handleDelete = workoutId => {
+    dispatch(deleteWorkout(workoutId))
+      .then(() => {
+        toast.success('Workout deleted successfully!');
+      })
+      .catch(error => {
+        toast.error('Failed to delete workout');
+      });
+  };
+
+  const handleViewDetails = workoutId => {
+    navigate(`/workouts/workout-detail/${workoutId}`);
+  };
 
   const formatDate = dateString => {
     const date = new Date(dateString);
@@ -40,19 +69,22 @@ const WorkoutList = () => {
     return `${hours}:${minutes}:${seconds} ${day}/${month}/${year}`;
   };
 
-  const handleDelete = workoutId => {
-    dispatch(deleteWorkout(workoutId))
-      .then(() => {
-        toast.success('workout delete successfully!');
-      })
-      .catch(error => {
-        toast.error('Failed to create plan');
-      });
+  const getMusclesAndEquipment = exerciseId => {
+    const exercise = exercises.find(ex => ex._id === exerciseId);
+
+    if (exercise) {
+      const muscles =
+        exercise.muscles?.map(muscle => muscle.title).join(', ') ||
+        'No muscles data';
+      const equipment =
+        exercise.equipments?.map(equipment => equipment.title).join(', ') ||
+        'No equipment data';
+
+      return { muscles, equipment };
+    }
+    return { muscles: 'Loading...', equipment: 'Loading...' };
   };
 
-  const handleViewDetails = workoutId => {
-    navigate(`/workouts/workout-detail/${workoutId}`);
-  };
   return (
     <div className='p-8 bg-gray-100'>
       <h2 className='text-3xl font-semibold mb-6 text-center'>
@@ -70,6 +102,7 @@ const WorkoutList = () => {
           <thead>
             <tr className='bg-gray-800 text-white'>
               <th className='px-6 py-4 text-left'>Workout Name</th>
+              <th className='px-6 py-4 text-left'>Image</th>
               <th className='px-6 py-4 text-left'>Exercises</th>
               <th className='px-6 py-4 text-left'>Muscles</th>
               <th className='px-6 py-4 text-left'>Equipment</th>
@@ -90,45 +123,44 @@ const WorkoutList = () => {
                   </span>
                 </td>
                 <td className='px-6 py-4'>
+                  <img
+                    src={workout.image || logo}
+                    alt={workout.title}
+                    className='w-20 h-20 object-cover rounded-md'
+                  />
+                </td>
+                <td className='px-6 py-4'>
                   <span className='inline-block bg-green-100 text-gray-800 px-4 py-2 rounded-full'>
                     {workout.exercises.length} exercises
                   </span>
                 </td>
                 <td className='px-6 py-4'>
-                  {workout.exercises.map(exercise => (
-                    <div key={exercise._id}>
-                      {exercise.exercise.muscles.map(muscleId => {
-                        const muscle = muscles.find(m => m._id === muscleId);
-                        return muscle ? (
-                          <span
-                            key={muscle._id}
-                            className='inline-block bg-gray-200 text-gray-700 px-4 py-1 rounded-full mr-2 mb-2'
-                          >
-                            {muscle.title}
-                          </span>
-                        ) : null;
-                      })}
-                    </div>
-                  ))}
+                  {workout.exercises?.map(exercise => {
+                    const { muscles } = getMusclesAndEquipment(
+                      exercise.exercise
+                    );
+                    return (
+                      <div key={exercise._id}>
+                        <span className='inline-block bg-gray-200 text-gray-700 px-4 py-1 rounded-full mr-2 mb-2'>
+                          {muscles}
+                        </span>
+                      </div>
+                    );
+                  })}
                 </td>
                 <td className='px-6 py-4'>
-                  {workout.exercises.map(exercise => (
-                    <div key={exercise._id}>
-                      {exercise.exercise.equipments.map(equipmentId => {
-                        const equipment = equipments.find(
-                          e => e._id === equipmentId
-                        );
-                        return equipment ? (
-                          <span
-                            key={equipment._id}
-                            className='inline-block bg-gray-200 text-gray-700 px-4 py-1 rounded-full mr-2 mb-2'
-                          >
-                            {equipment.title}
-                          </span>
-                        ) : null;
-                      })}
-                    </div>
-                  ))}
+                  {workout.exercises?.map(exercise => {
+                    const { equipment } = getMusclesAndEquipment(
+                      exercise.exercise
+                    );
+                    return (
+                      <div key={exercise._id}>
+                        <span className='inline-block bg-gray-200 text-gray-700 px-4 py-1 rounded-full mr-2 mb-2'>
+                          {equipment}
+                        </span>
+                      </div>
+                    );
+                  })}
                 </td>
                 <td className='px-6 py-4'>{formatDate(workout.createdAt)}</td>
                 <td className='px-6 py-4'>
@@ -147,6 +179,37 @@ const WorkoutList = () => {
           </tbody>
         </table>
       </div>
+
+      {totalPages > 1 && (
+        <Pagination className='mt-8'>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+              />
+            </PaginationItem>
+
+            {Array.from({ length: totalPages }, (_, index) => (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  isActive={currentPage === index + 1}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationNext
+                onClick={() =>
+                  setCurrentPage(prev => Math.min(prev + 1, totalPages))
+                }
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
     </div>
   );
 };
