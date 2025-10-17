c2a65d5ad3ab60452a68e3b19714344a95b037e9
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-17 22:43:33 +0700

    chore: header of workout session

--- client/src/components/workout-session.jsx
+++ client/src/components/workout-session.jsx
@@ -51,7 +51,7 @@ const WorkoutSession = () => {
 
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col justify-between'>
-      <header className='flex justify-between items-center px-6 py-4 border-b sticky top-0 bg-white z-10'>
+      <header className='flex justify-between items-center px-6 py-4 border-b sticky top-0 z-10'>
         <div>
           <h2 className='text-2xl text-gray-600'>
             {currentWorkout.title || 'Workout Session'}
