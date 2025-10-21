92c7b6ea8a72c44e1d16bce2d5066070ca3f6fb9
lnvh <lenguyenviethung1509@gmail.com>
2025-10-21 09:28:38 +0700

    fix: resolve food provider

--- client/src/components/admin/foods/foods-provider.jsx
+++ client/src/components/admin/foods/foods-provider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const FoodsContext = createContext();
 
@@ -14,31 +14,20 @@ export const FoodsProvider = ({ children }) => {
   const [selectedFood, setSelectedFood] = useState(null);
   const [selectedFoods, setSelectedFoods] = useState([]);
   const [isViewDialogOpen, setIsViewDialogOpen] = useState(false);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-  const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
-  const [isMultiDeleteDialogOpen, setIsMultiDeleteDialogOpen] = useState(false);
+
+  const value = {
+    selectedFood,
+    setSelectedFood,
+    selectedFoods,
+    setSelectedFoods,
+    isViewDialogOpen,
+    setIsViewDialogOpen,
+    isDeleteDialogOpen,
+    setIsDeleteDialogOpen
+  };
 
   return (
-    <FoodsContext.Provider
-      value={{
-        selectedFood,
-        setSelectedFood,
-        selectedFoods,
-        setSelectedFoods,
-        isViewDialogOpen,
-        setIsViewDialogOpen,
-        isEditDialogOpen,
-        setIsEditDialogOpen,
-        isDeleteDialogOpen,
-        setIsDeleteDialogOpen,
-        isCreateDialogOpen,
-        setIsCreateDialogOpen,
-        isMultiDeleteDialogOpen,
-        setIsMultiDeleteDialogOpen
-      }}
-    >
-      {children}
-    </FoodsContext.Provider>
+    <FoodsContext.Provider value={value}>{children}</FoodsContext.Provider>
   );
 };
