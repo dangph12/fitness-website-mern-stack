58ada7b7b2915fb9b3761b9924fc2c8c18256c54
Le Nguyen Viet Hung <lenguyenviethung1509@gmail.com>
2025-10-24 02:11:45 +0700

    fix: responsive table

--- client/src/components/admin/users/users-table.jsx
+++ client/src/components/admin/users/users-table.jsx
@@ -363,28 +363,18 @@ export function UsersTable() {
       )}
 
       {/* Table */}
-      <div className='rounded-md border'>
+      <div className='rounded-md border w-full overflow-x-auto'>
         <Table>
           <TableHeader>
             {table.getHeaderGroups().map(headerGroup => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map(header => (
                   <TableHead key={header.id}>
-                    {header.isPlaceholder ? null : header.column.getCanSort() ? (
-                      <Button
-                        variant='ghost'
-                        onClick={() => header.column.toggleSorting()}
-                        className='p-0 h-auto font-medium hover:bg-transparent'
-                      >
-                        {typeof header.column.columnDef.header === 'function'
-                          ? header.column.columnDef.header(header.getContext())
-                          : header.column.columnDef.header}
-                      </Button>
-                    ) : typeof header.column.columnDef.header === 'function' ? (
-                      header.column.columnDef.header(header.getContext())
-                    ) : (
-                      header.column.columnDef.header
-                    )}
+                    {header.isPlaceholder
+                      ? null
+                      : typeof header.column.columnDef.header === 'function'
+                        ? header.column.columnDef.header(header.getContext())
+                        : header.column.columnDef.header}
                   </TableHead>
                 ))}
               </TableRow>
