470f1a0881ef18ef9089976c8eea5a0264d9017f
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-28 08:24:21 +0700

    fix: create meal user

--- client/src/components/user-card.jsx
+++ client/src/components/user-card.jsx
@@ -5,16 +5,16 @@ const UserCard = ({ user }) => {
     <div className='flex items-center p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-all'>
       <div className='w-16 h-16 rounded-full overflow-hidden border-2 border-gray-300'>
         <img
-          src={user.avatar}
-          alt={`${user.name}'s avatar`}
+          src={user?.avatar}
+          alt={`${user?.name}'s avatar`}
           className='w-full h-full object-cover'
         />
       </div>
 
       <div className='ml-4'>
         <p className='text-sm text-gray-500'>Meal Creator</p>
-        <p className='text-lg font-semibold text-gray-800'>{user.name}</p>
-        <p className='text-sm text-gray-600'>{user.email}</p>
+        <p className='text-lg font-semibold text-gray-800'>{user?.name}</p>
+        <p className='text-sm text-gray-600'>{user?.email}</p>
       </div>
     </div>
   );
