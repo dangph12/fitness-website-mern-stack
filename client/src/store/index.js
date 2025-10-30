6b5633f2a42c98f4a2f7a9509fe3b8509eb82ed4
Trang Vu <144322664+vulinhtrang2305@users.noreply.github.com>
2025-10-30 13:01:37 +0700

    feat: goal, meal, bodyrecord in index

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '~/store/features/auth-slice';
 import avatarReducer from '~/store/features/avatar-slice';
+import bodyRecordsReducer from '~/store/features/body-records-slice';
 import equipmentsReducer from '~/store/features/equipment-slice';
 import exercisesReducer from '~/store/features/exercise-slice';
 import favouritesReducer from '~/store/features/favourite-slice';
 import foodReducer from '~/store/features/food-slice';
 import goalsReducer from '~/store/features/goal-slice';
 import historiesReducer from '~/store/features/history-slice';
+import mealAisReducer from '~/store/features/meal-ai-slice';
 import mealsReducer from '~/store/features/meal-slice';
 import musclesReducer from '~/store/features/muscles-slice';
 import plansReducer from '~/store/features/plan-slice';
@@ -28,6 +30,8 @@ export default configureStore({
     plans: plansReducer,
     histories: historiesReducer,
     favourites: favouritesReducer,
-    goals: goalsReducer
+    goals: goalsReducer,
+    mealAi: mealAisReducer,
+    bodyRecords: bodyRecordsReducer
   }
 });
