951d05c4736b81895b74c4541d2fbee7d0b3d3f2
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-11-07 10:43:24 +0700

    fix: remove body classification module

--- server/src/modules/body-records/body-record-model.ts
+++ server/src/modules/body-records/body-record-model.ts
@@ -7,12 +7,7 @@ const BodyRecordSchema = new Schema<IBodyRecord>(
     height: { type: Number, required: true },
     weight: { type: Number, required: true },
     bmi: { type: Number, required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    bodyClassification: {
-      type: Schema.Types.ObjectId,
-      ref: 'BodyClassification',
-      required: false
-    }
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
   },
   { timestamps: true }
 );
