5d8b19a01e28e7a9b012a0f26ff9bc4692225a7a
dangph12 <dangphhe181217@fpt.edu.vn>
2025-11-13 13:02:39 +0700

    feat: change script to build app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    preview: {
+      port: 80,
+      allowedHosts: [env.VITE_URL]
+    }
+  };
 });
